3d360783eb1b83420a79b9505c87bcec83d08a15
Ariel Reyes <alreyes2@espe.edu.ec>
2026-06-07 21:43:34 -0500

    chore: setup environment.example.ts para GitHub Actions

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  // Esta es la URL de tu backend. En producción o GitHub Actions
+  // esto puede ser reemplazado por la URL real.
+  apiUrl: 'http://localhost:8080'
+};
