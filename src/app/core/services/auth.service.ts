419c374c8b6cd0bd041f9935104bf58aafe4c716
Ariel Reyes <alreyes2@espe.edu.ec>
2026-06-07 21:29:33 -0500

    fix: remove internal angular git init and track real web architecture source files

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable, tap } from 'rxjs';
-import { environment } from '../../../environments/environment';
+import { environment } from '../../../env';
 import { User } from '../models/user.model';
 
 @Injectable({
